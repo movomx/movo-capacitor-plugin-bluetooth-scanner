189a3c635eea7124d924ea80a262150c10659d45
alex <alex.guajardo@inroute.com.mx>
2023-11-14 09:50:48 -0700

    Fixing typo in DeviceFilter

--- android/src/main/java/com/movomx/bluetooth/scanner/DeviceFilter.java
+++ android/src/main/java/com/movomx/bluetooth/scanner/DeviceFilter.java
@@ -44,6 +44,6 @@ public class DeviceFilter {
   }
 
   private String format(String word) {
-    return word.toLowwerCase().replaceAll(":", "");
+    return word.toLowerCase().replaceAll(":", "");
   }
 }
